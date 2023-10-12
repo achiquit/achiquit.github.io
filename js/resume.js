4f119ea7350fac00e241b74cbe1b767d8435b414
Andre Chiquito <andre.chiquito@gmail.com>
2023-10-12 15:32:10 -0400

    First Ascents tile

---
+++ js/resume.js
@@ -0,0 +1,10 @@
+console.log('Resume JS file is loaded.');
+
+$( document ).ready(function() {
+    console.log( "Jquery ready!" );
+    
+    $('#everything-btn').click(function(){
+     console.log('testing');
+    });
+    
+});
